62b48d7becdef264cd2f4aaa9dbf0ff1fa4e61de
NatyNatur <naticcg@gmail.com>
2020-01-13 15:24:36 -0300

    corrections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,15 @@
-(function ($) {
-    $(document).ready(function () {
-        $.jScrollability([{
-                'selector': '.first-wine div img',
-                'start': 'parent',
-                'end': 'parent',
-                'fn': {
-                    'right': {
-                        'start': 50,
-                        'end': 0,
-                        'unit': '%'
-                    }
-                }
-            },
-            {
-                'selector': '.second-wine div img',
-                'start': 'parent',
-                'end': 'parent',
-                'fn': {
-                    'left': {
-                        'start': 50,
-                        'end': 0,
-                        'unit': '%'
-                    }
-                }
-            },
-            {
-                'selector': '.third-wine div img',
-                'start': 'parent',
-                'end': 'parent',
-                'fn': {
-                    'right': {
-                        'start': 50,
-                        'end': 0,
-                        'unit': '%'
-                    }
-                }
-            },
-            {
-                'selector': '.fourth-wine div img',
-                'start': 'parent',
-                'end': 'parent',
-                'fn': {
-                    'left': {
-                        'start': 50,
-                        'end': 0,
-                        'unit': '%'
-                    }
-                }
-            }
-        ]);
-    });
-})(jQuery);
-
-
 function goToTop() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
 }
+
+function goTo(des) {
+    var destination = des.target;
+    // TODO: smooth scroll 
+}
+
+$('html').on('click', function () {
+    $('.collapse').collapse('hide')
+})
